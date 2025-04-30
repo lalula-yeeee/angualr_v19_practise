4a7f5da5f0f193079c80a32ac9aa9b06e57dad9a
Sylvia Chen <kelly901021@digiwin.com>
2025-04-30 22:42:45 +0800

    [feat]建立基本路由

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './core/first/first.component';
+import { SecondComponent } from './core/second/second.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'first', component: FirstComponent},
+  { path: 'second', component: SecondComponent}
+];
