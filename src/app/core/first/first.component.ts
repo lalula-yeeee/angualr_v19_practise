ebf21c561f582d38af0d3e333dff50813e101296
Sylvia Chen <kelly901021@digiwin.com>
2025-06-12 14:51:43 +0800

    [feat]嘗試操作signal

--- src/app/core/first/first.component.ts
+++ src/app/core/first/first.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-first',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './first.component.html',
   styleUrl: './first.component.less'
 })
 export class FirstComponent {
+  firstName = signal('Morgan');
+  age = signal(20);
+  updateCount = signal(0);
 
+  lastName = '1234';
+  birthday: Date = new Date();
+
+  setFirstName() {
+    this.firstName.set('Jason');
+    this.setUpdateCount();
+  }
+
+  computeAge() {
+    const date = new Date(this.birthday);
+    this.age.set( this.calculateAge(date) );
+    this.setUpdateCount();
+  }
+
+  calculateAge(birthDate: Date): number {
+    const today = new Date();
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    const hasBirthdayPassedThisYear =
+      today.getMonth() > birthDate.getMonth() ||
+      (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate());
+
+    if (!hasBirthdayPassedThisYear) {
+      age--;
+    }
+
+    return age;
+  }
+
+  setUpdateCount() {
+    this.updateCount.set(this.updateCount() + 1);
+  }
 }
